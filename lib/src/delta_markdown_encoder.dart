bd33921a35ef17c4a61376a426af744c4ec9ba83
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-12-31 02:46:02 +0100

    write checkmark list items

--- lib/src/delta_markdown_encoder.dart
+++ lib/src/delta_markdown_encoder.dart
@@ -243,6 +243,10 @@ class DeltaMarkdownEncoder extends Converter<String, String> {
       buffer.write('* ');
     } else if (block == Attribute.ol) {
       buffer.write('1. ');
+    } else if (block == Attribute.checked) {
+      buffer.write('– [x] ');
+    } else if (block == Attribute.unchecked) {
+      buffer.write('– [ ] ');
     } else if (block.key == Attribute.h1.key && block.value == 1) {
       buffer.write('# ');
     } else if (block.key == Attribute.h2.key && block.value == 2) {
